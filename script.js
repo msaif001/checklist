c1dcc4a1190099260cb58d0107b5ca7a94317fe5
Mohammed saif <saifmohammed888@gmail.com>
2023-11-08 23:31:16 +0530

    fix

--- script.js
+++ script.js
@@ -1,32 +1,21 @@
-let listItem=document.getElementById("listItem")
 let listContainer=document.getElementById("listContainer")
-listItem.style.display = 'flex';
-listContainer.appendChild(listItem)
-let listItem2=document.getElementById("listItem")
-listItem2.style.display = 'flex';
-listItem2.setAttribute("key","list")
-console.log(listItem2)
-listContainer.appendChild(listItem2)
-console.log(listContainer)
+let listItem=document.getElementById("listItem")
 
+let inputElement = document.getElementById("textInput")
 
-
-
-
-let clear= document.getElementById("textInput")
-
-let todoArray=[ ]
 function plus(){
-     let store = clear.value
-     todoArray.push(store)
-     clear.value=""
-     showListItem()
-}
-
-function showListItem(){
-     for(i=0; i<todoArray.length;i++){
-          console.log(todoArray[i])
+     if(inputElement.value !== ''){
+          let clonedItem = listItem.cloneNode(true);
+          clonedItem.style.display = 'flex';
+          clonedItem.querySelector("#first").innerText = inputElement.value
+          listContainer.appendChild(clonedItem)
+          inputElement.value = ""
+     }else{
+          alert("Please enter before submit")
      }
 }
 
-
+function deleteItem(x){
+     let element = x.parentNode
+     element.remove()
+}
